af50f288158eebbf50ebbe1a0af10fbc8f145ee7
kunalavghade <kunalavghade77@example.com>
2022-04-30 16:30:13 +0530

    v1

--- DAS/DAS/DAS.c
+++ DAS/DAS/DAS.c
@@ -7,18 +7,35 @@
 #define PT100 0
 #define WEIGHT 1
 
+unsigned long voltage;
+
 int main(void)
 {
-	char Msg[10];
+	char Msg1[] = "TEMP: 0.000 Volts";
+	char Msg2[] = "WEG : 0.000 volts";
 	Initialsie_LCD();
 	LCD_Message(1,0,"INITIALIZING...");
 	adc_init();
     while(1)
     {
         LCD_clear();
-		sprintf(Msg,"MOISTURE: %f",getVal(PT100));
+		
+		voltage = analogRead(PT100);
+		
+		Msg1[6]=(voltage/1000)+48;
+		Msg1[7]=(voltage%1000)/100+0x30;
+		Msg1[8]=(voltage%100)/10+0x30;
+		Msg1[9]=(voltage%10) + 0x30;
+		
 		LCD_Message(1,0,Msg);
-		sprintf(Msg,"TEMPERATURE: %d",getVal(WEIGHT));
+		
+		voltage = analogRead(PT100);
+		
+		Msg2[6]=(voltage/1000)+48;
+		Msg2[7]=(voltage%1000)/100+0x30;
+		Msg2[8]=(voltage%100)/10+0x30;
+		Msg2[9]=(voltage%10) + 0x30;
+		
 		LCD_Message(2,0,Msg);
 		_delay_ms(25);
     }
